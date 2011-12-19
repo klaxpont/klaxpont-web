89d443345381f9c8c4452e278fdd20c14d05d517
Fred <arnoux.frederic@gmail.com>
2011-12-20 00:34:00 +0100

    Go rails !

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KlaxpontWeb::Application.config.secret_token = 'c11736b466df06acec6690ff5daeb2d2f18acbf9a8b6ecd414bca7ff88653467fcff4b080e34d54a7b9476dcbf2f29ebabb9206a601b4992ea78ca1561e7ac77'
