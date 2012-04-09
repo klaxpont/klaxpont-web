24f5346ad90a62b88242514e4f672428a7a63cb2
Fred <arnoux.frederic@gmail.com>
2012-04-09 16:08:23 +0200

    Global error handling in dailymotion_controller.

--- app/controllers/dailymotion_controller.rb
+++ app/controllers/dailymotion_controller.rb
@@ -1,33 +1,33 @@
 class DailymotionController < ApplicationController
   
   require "json"
-
   respond_to :json
+
+  rescue_from Exception, :with => :error_render_method
+
+  def error_render_method(e)
+    print e.backtrace.join("\n") + "\n"
+    print e.message
+    json = { :error => true, :message => e.message }.to_json
+    respond_with json
+    true
+  end
   
 
   def get_token
     # Retrieve the `refresh_token` from the db.
     refresh_token = Dailymotion.get_refresh_token
 
-    begin
-      response = DailymotionApi.get_token(refresh_token)
+    response = DailymotionApi.get_token(refresh_token)
 
-      unless response.success?
-        raise response.response
-      end
+    # unless response.success?
+    #   raise Exception.new response.response
+    # end
 
-      # Save the `refresh_token`.
-      Dailymotion.save_refresh_token response["refresh_token"]
+    # Save the `refresh_token`.
+    Dailymotion.save_refresh_token response["refresh_token"]
 
-      respond_with response.body
-
-    rescue Exception => e
-      print e.backtrace.join("\n") + "\n"
-      print e.message
-      json = { :error => true, :message => e.message }.to_json
-      respond_with json
-    end
-
+    respond_with response.body
   end
 
 
