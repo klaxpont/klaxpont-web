e7f9d3686e77e952c9ad21a4a45528e70f2b31fd
François Benaiteau <francois.benaiteau@gmail.com>
2012-06-15 00:42:17 +0200

    add dailymotion_api to autoload path

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module Klaxpont
     
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-
+    config.autoload_paths += %W(lib)
+    #config.autoload_paths << "lib/dailymotion_api.rb"
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
