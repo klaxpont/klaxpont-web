e981223f928c7d288df2c319b6388828a5088f1f
François Benaiteau <francois.benaiteau@gmail.com>
2012-06-15 00:41:39 +0200

    raise error if credentials are empty

--- config/initializers/load_credentials.rb
+++ config/initializers/load_credentials.rb
@@ -1,3 +1,8 @@
 Klaxpont::Application.configure do
   config.dailymotion_credentials = YAML.load(ERB.new(File.read("#{Rails.root}/config/credentials.yml")).result)["dailymotion"]
+  config.dailymotion_credentials.each do |k,v|
+  	if v.nil? or v.empty?
+		raise "daylimotion credentials '#{k}' is invalid"
+  	end
+  end
 end
