fad8ba3d878b89998f1eda4529a37346dc321939
François Benaiteau <francois.benaiteau@gmail.com>
2012-07-09 23:59:40 +0200

    add condition for running tests on CI server without dailymotion credentials

--- config/initializers/load_credentials.rb
+++ config/initializers/load_credentials.rb
@@ -1,8 +1,9 @@
 Klaxpont::Application.configure do
   config.dailymotion_credentials = YAML.load(ERB.new(File.read("#{Rails.root}/config/credentials.yml")).result)["dailymotion"]
+
   config.dailymotion_credentials.each do |k,v|
   	if v.nil? or v.empty?
-		raise "daylimotion credentials '#{k}' is invalid"
+		  raise "daylimotion credentials '#{k}' is invalid" unless Rails.env.test?
   	end
   end
 end
