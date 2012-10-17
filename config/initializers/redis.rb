cb4d60f767412abc401838a039c9e2a538ac873e
François Benaiteau <francois.benaiteau@gmail.com>
2012-10-17 18:50:13 +0200

    FIX Redis droppping connection on heroku - bad instantiation

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,7 @@
-Klaxpont::Application.configure do
-  config.redis_settings = YAML.load(ERB.new(File.read(Rails.root.join("config", "redis.yml"))).result)[Rails.env]
+redis_settings = YAML.load(ERB.new(File.read(Rails.root.join("config", "redis.yml"))).result)[Rails.env]
+if(redis_settings['url'])
+  uri = URI.parse(redis_settings['url'])
+else
+  uri = OpenStruct.new(:host => redis_settings['host'], :port => redis_settings['port'], :password => redis_settings['password'])
 end
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- lib/dailymotion_wrapper.rb
+++ lib/dailymotion_wrapper.rb
@@ -11,13 +11,11 @@ module DailymotionWrapper
 
 
   def get_access_token
-    redis = Redis.new(Klaxpont::Application.config.redis_settings)
-
     # Get token from Redis
-    access_token = redis.get(REDIS_KEY[:access_token])
+    access_token = REDIS.get(REDIS_KEY[:access_token])
 
     if access_token.nil?
-      refresh_token = redis.get(REDIS_KEY[:refresh_token])
+      refresh_token = REDIS.get(REDIS_KEY[:refresh_token])
 
       # Get token from dailymotion
       response = DailymotionApi.get_token(refresh_token)
@@ -25,9 +23,9 @@ module DailymotionWrapper
       seconds      = response["expires_in"]
       access_token = response["access_token"]
 
-      redis.multi do
-        redis.setex REDIS_KEY[:access_token], seconds, access_token
-        redis.setex REDIS_KEY[:refresh_token], seconds, response["refresh_token"]
+      REDIS.multi do
+        REDIS.setex REDIS_KEY[:access_token], seconds, access_token
+        REDIS.setex REDIS_KEY[:refresh_token], seconds, response["refresh_token"]
       end
     end
 
