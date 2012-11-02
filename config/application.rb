db0491073b8537c7f56cdd29ff89682b773160ee
François Benaiteau <francois.benaiteau@gmail.com>
2012-11-02 19:25:42 +0100

    FIX cannot load dailymotion api module in production

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Klaxpont
     
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(lib)
+    config.autoload_paths += %W(#{config.root}/lib)
     #config.autoload_paths << "lib/dailymotion_api.rb"
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,6 +2,7 @@ redis_settings = YAML.load(ERB.new(File.read(Rails.root.join("config", "redis.ym
 if(redis_settings['url'])
   uri = URI.parse(redis_settings['url'])
 else
+  require 'ostruct'
   uri = OpenStruct.new(:host => redis_settings['host'], :port => redis_settings['port'], :password => redis_settings['password'])
 end
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
