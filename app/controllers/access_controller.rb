3ba91c465ab4f38a1a17f4245ce0e720ffe955aa
François Benaiteau <francois.benaiteau@gmail.com>
2013-07-07 16:53:26 +0200

    Adapt json response for iOS client include client api key and secret

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -3,9 +3,13 @@ class AccessController < ApplicationController
   require "dailymotion_wrapper"
 
   respond_to :json
-  
+
   def get_token
     @access_token = DailymotionWrapper.get_access_token
+
+    credentials = Klaxpont::Application.config.dailymotion_credentials
+    @api_key = credentials["client_id"]
+    @api_secret = credentials["client_secret"]
   end
 
 end
--- app/views/access/get_token.json.jbuilder
+++ app/views/access/get_token.json.jbuilder
@@ -1,5 +1,7 @@
 json.response do |body|
   body.access_token @access_token
+  body.api_key @api_key
+  body.api_secret @api_secret
 end
 
 
