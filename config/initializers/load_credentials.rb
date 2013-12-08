7ff7f3eb70accd8fe7d99713b962d6de29312e88
netbe <francois.benaiteau@gmail.com>
2013-12-08 22:39:15 +0100

    Disable load env.
    Failing during assets precompilation with error:
    undefined method `empty?' for nil:NilClass

--- config/initializers/load_credentials.rb
+++ config/initializers/load_credentials.rb
@@ -3,7 +3,7 @@ Klaxpont::Application.configure do
 
   config.dailymotion_credentials.each do |k,v|
   	if v.nil? or v.empty?
-		  raise "daylimotion credentials '#{k}' is invalid" unless Rails.env.test?
+		  # raise "daylimotion credentials '#{k}' is invalid" unless Rails.env.test?
   	end
   end
 end
